daeffcb65de91cee31ae95decc621e49f5dc912c
Leetattoo <leandrodeoliveiracontreras@hotmail.com>
2026-09-03 10:08:17 -0300

    v1.3: load hard-audit rebuild modules

---
+++ bootstrap-v13.js
@@ -0,0 +1,7 @@
+await import('./app-v09.js?v=20260903-v13');
+await import('./patch-v10.js?v=20260903-v13');
+await import('./patch-v11.js?v=20260903-v13');
+await import('./patch-v12.js?v=20260903-v13');
+await import('./patch-v13-ground.js?v=20260903-v13');
+await import('./patch-v13-social.js?v=20260903-v13');
+await import('./patch-v13-stairs-audit.js?v=20260903-v13');
