82b7657875135a1ee615f2cffa6c3ace4c120941
Leetattoo <leandrodeoliveiracontreras@hotmail.com>
2026-09-03 07:49:56 -0300

    Integra auditoria dimensional v0.7 ao bootstrap

--- bootstrap-v06.js
+++ bootstrap-v06.js
@@ -1,8 +1,13 @@
-import './prelude-v06.js';
+import './prelude-v06.js?v=20260903-scale-v07';
 await import('./app-final.js?v=20260903-final');
 const { installCasaContrerasV06 } = await import('./enhancements-v06.js?v=20260903-v06');
 installCasaContrerasV06({
   scene: window.__CASA_SCENE__,
   camera: window.__CASA_CAMERA__,
   renderer: window.__CASA_RENDERER__,
+});
+const { installDimensionAuditV07 } = await import('./dimension-audit-v07.js?v=20260903-v07');
+installDimensionAuditV07({
+  scene: window.__CASA_SCENE__,
+  camera: window.__CASA_CAMERA__,
 });
