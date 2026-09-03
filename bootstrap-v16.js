ecc97f4090d433e03a5ac638ccd497981e03bb33
Leetattoo <leandrodeoliveiracontreras@hotmail.com>
2026-09-03 11:30:38 -0300

    load v1.6 finalize placement fix

--- bootstrap-v16.js
+++ bootstrap-v16.js
@@ -2,3 +2,4 @@ await import('./app-v09.js?v=20260903-v16');
 await import('./patch-v10.js?v=20260903-v16');
 await import('./patch-v15-reality.js?v=20260903-v16');
 await import('./patch-v16-bughunt.js?v=20260903-v16');
+await import('./patch-v16-finalize.js?v=20260903-v16');
