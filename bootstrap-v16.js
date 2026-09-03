e62f03bc930e1bc38f67eb9a3c9d43ffb7dc55f2
Leetattoo <leandrodeoliveiracontreras@hotmail.com>
2026-09-03 11:27:02 -0300

    bootstrap v1.6 heavy bug hunt

---
+++ bootstrap-v16.js
@@ -0,0 +1,4 @@
+await import('./app-v09.js?v=20260903-v16');
+await import('./patch-v10.js?v=20260903-v16');
+await import('./patch-v15-reality.js?v=20260903-v16');
+await import('./patch-v16-bughunt.js?v=20260903-v16');
