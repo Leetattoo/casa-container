6674d92913fc7aaddc1b556e68df414af76a44fb
Leetattoo <leandrodeoliveiracontreras@hotmail.com>
2026-09-03 10:35:54 -0300

    v1.4: ativar correção de colisão lateral

--- bootstrap-v14.js
+++ bootstrap-v14.js
@@ -3,3 +3,4 @@ await import('./patch-v10.js?v=20260903-v14');
 await import('./patch-v13-ground.js?v=20260903-v14');
 await import('./patch-v13-social.js?v=20260903-v14');
 await import('./patch-v14-clean.js?v=20260903-v14');
+await import('./patch-v14-collision.js?v=20260903-v14');
