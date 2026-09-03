c4ae66017ddb1bbe9251dfd46832001b5f74f0ae
Leetattoo <leandrodeoliveiracontreras@hotmail.com>
2026-09-03 10:34:10 -0300

    v1.4: bootstrap limpo sem camadas v11/v12 conflitantes

---
+++ bootstrap-v14.js
@@ -0,0 +1,5 @@
+await import('./app-v09.js?v=20260903-v14');
+await import('./patch-v10.js?v=20260903-v14');
+await import('./patch-v13-ground.js?v=20260903-v14');
+await import('./patch-v13-social.js?v=20260903-v14');
+await import('./patch-v14-clean.js?v=20260903-v14');
