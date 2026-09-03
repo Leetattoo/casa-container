47799a17bd97f2b5f7002955844f2908f3aebf65
Leetattoo <leandrodeoliveiracontreras@hotmail.com>
2026-09-03 13:15:29 -0300

    activate v1.8 access space QA

--- bootstrap-v16.js
+++ bootstrap-v16.js
@@ -1,6 +1,7 @@
-await import('./app-v09.js?v=20260903-v17d');
-await import('./patch-v10.js?v=20260903-v17d');
-await import('./patch-v15-reality.js?v=20260903-v17d');
-await import('./patch-v16-bughunt.js?v=20260903-v17d');
-await import('./patch-v16-finalize.js?v=20260903-v17d');
-await import('./patch-v17-spatial-audit.js?v=20260903-v17d');
+await import('./app-v09.js?v=20260903-v18');
+await import('./patch-v10.js?v=20260903-v18');
+await import('./patch-v15-reality.js?v=20260903-v18');
+await import('./patch-v16-bughunt.js?v=20260903-v18');
+await import('./patch-v16-finalize.js?v=20260903-v18');
+await import('./patch-v17-spatial-audit.js?v=20260903-v18');
+await import('./patch-v18-access-space.js?v=20260903-v18');
