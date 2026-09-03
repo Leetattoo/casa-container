cf2ab4228269678b2c13447e06a76fbdb809fbf8
Leetattoo <leandrodeoliveiracontreras@hotmail.com>
2026-09-03 09:46:06 -0300

    v1.2: carregar revisão de fidelidade e detalhes

---
+++ bootstrap-v12.js
@@ -0,0 +1,4 @@
+await import('./app-v09.js?v=20260903-v12');
+await import('./patch-v10.js?v=20260903-v12');
+await import('./patch-v11.js?v=20260903-v12');
+await import('./patch-v12.js?v=20260903-v12');
