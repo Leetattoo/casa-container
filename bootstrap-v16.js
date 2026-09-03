b4c2f9040c28659fa40a3c0a3238afbde688d862
Leetattoo <leandrodeoliveiracontreras@hotmail.com>
2026-09-03 13:09:06 -0300

    bust v1.7 final audit cache

--- bootstrap-v16.js
+++ bootstrap-v16.js
@@ -1,6 +1,6 @@
-await import('./app-v09.js?v=20260903-v17');
-await import('./patch-v10.js?v=20260903-v17');
-await import('./patch-v15-reality.js?v=20260903-v17');
-await import('./patch-v16-bughunt.js?v=20260903-v17');
-await import('./patch-v16-finalize.js?v=20260903-v17');
-await import('./patch-v17-spatial-audit.js?v=20260903-v17');
+await import('./app-v09.js?v=20260903-v17d');
+await import('./patch-v10.js?v=20260903-v17d');
+await import('./patch-v15-reality.js?v=20260903-v17d');
+await import('./patch-v16-bughunt.js?v=20260903-v17d');
+await import('./patch-v16-finalize.js?v=20260903-v17d');
+await import('./patch-v17-spatial-audit.js?v=20260903-v17d');
